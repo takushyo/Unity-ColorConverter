8366385264d35f4ea79ec6fbd675213f7397fa18
takushyo <50485193+takushyo@users.noreply.github.com>
2022-08-14 00:10:10 +1000

    refactors to do bulk edit

--- Converter.ps1
+++ Converter.ps1
@@ -1,16 +1,18 @@
-$file = Get-Content .\RGB-Table
-$filename = Read-Host -Prompt "filename without extension?"
-New-Item -ItemType Directory -Force -Path ".\exported" >> $null
-$path = ".\exported\$filename.colors"
-Out-File $path
-$convertedList = Get-Content ".\template.txt" | Out-String
-foreach($row in $file){
-    $line = $row.Split("`t")
-    $line[0] = [System.Math]::Round([decimal]$line[0] / 255.0, 6)
-    $line[1] = [System.Math]::Round([decimal]$line[1] / 255.0, 6)
-    $line[2] = [System.Math]::Round([decimal]$line[2] / 255.0, 6)
-    $convertedList += "  - m_Name: `n"
-    $convertedList += @("    m_Color: {r: " + $line[0]+", g: "+$line[1]+", b: "+$line[2] + ", a: 1}`n")
+Remove-Item .\Export\* -Recurse -Force
+New-Item -ItemType Directory -Force -Path ".\Export" >> $null
+Get-ChildItem ".\Import" -Filter *.pal |
+ForEach-Object {
+    $path = ".\Export\"+$_.BaseName+".colors"
+    $convertedList = Get-Content ".\template.txt" | Out-String
+    $file = gc ".\Import\$_" | Select-Object -Skip 3 
+    foreach($row in $file){
+        $line = $row.Split(" ")
+        $line[0] = [System.Math]::Round([decimal]$line[0] / 255.0, 6)
+        $line[1] = [System.Math]::Round([decimal]$line[1] / 255.0, 6)
+        $line[2] = [System.Math]::Round([decimal]$line[2] / 255.0, 6)
+        $convertedList += "  - m_Name: `n"
+        $convertedList += @("    m_Color: {r: " + $line[0]+", g: "+$line[1]+", b: "+$line[2] + ", a: 1}`n")
+    }
+    $convertedList | Out-File -Encoding utf8 $path
 }
-$convertedList | Out-File -Encoding utf8 $path
 
