f69d3a0ccbc61d03dcbf9b14d953dbc8a06c1c52
takushyo <50485193+takushyo@users.noreply.github.com>
2022-08-13 23:37:36 +1000

    consolodated code a bit

--- Converter.ps1
+++ Converter.ps1
@@ -1,6 +1,9 @@
 $file = Get-Content .\RGB-Table
 $filename = Read-Host -Prompt "filename without extension?"
-$convertedList
+New-Item -ItemType Directory -Force -Path ".\exported" >> $null
+$path = ".\exported\$filename.colors"
+Out-File $path
+$convertedList = Get-Content ".\template.txt" | Out-String
 foreach($row in $file){
     $line = $row.Split("`t")
     $line[0] = [System.Math]::Round([decimal]$line[0] / 255.0, 6)
@@ -9,10 +12,5 @@ foreach($row in $file){
     $convertedList += "  - m_Name: `n"
     $convertedList += @("    m_Color: {r: " + $line[0]+", g: "+$line[1]+", b: "+$line[2] + ", a: 1}`n")
 }
-
-Out-File .\converted.txt
-Add-Content .\converted.txt @(Get-Content .\template)
-Add-Content .\converted.txt $convertedList
-New-Item -ItemType Directory -Force -Path ".\exported" >> $null
-Get-Content .\converted.txt | Set-Content -Encoding utf8 .\exported\"$filename.colors"
+$convertedList | Out-File -Encoding utf8 $path
 
